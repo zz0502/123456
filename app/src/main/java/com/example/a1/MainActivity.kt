2e801eb2922e9ede23fcad0cbb254d08f40c2eb6
student <david925233@gmail.com>
2023-05-31 11:37:16 +0800

    第二次comit

--- app/src/main/java/com/example/a1/MainActivity.kt
+++ app/src/main/java/com/example/a1/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.a1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.a1.ui.theme.蔡榮宏Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,16 +24,27 @@ class MainActivity : ComponentActivity() {
             蔡榮宏Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("蔡榮宏")
                 }
             }
         }
     }
 }
 
-
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+
+            Text(
+                text = "作者:資管二B $name",
+                fontSize = 25.sp
+            )
+            Image(
+                painter = painterResource(id = R.drawable.map),
+                contentDescription = "地圖"
+
+            )
+        }
+
 }
 
